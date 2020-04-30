6e2395239062db8f0837407e60f331232034226e
Jay4869 <jay4869@gmail.com>
2020-04-29 23:57:36 -0700

    update normalized cases

--- Projects/COVID-19/Data-Processing.py
+++ Projects/COVID-19/Data-Processing.py
@@ -8,45 +8,40 @@ def ETL(file):
     # loading COVID-19 data and zipcode
     print('Processing {}'.format(file[-14:]))
     data = pd.read_csv(file)
-    uszips = pd.read_csv('uszips.csv')
-    Geo = uszips.groupby(['county_fips', 'county_name', 'state_name'])['lat', 'lng'].agg('mean').reset_index()
-    Pop = uszips.groupby(['county_fips', 'county_name', 'state_name']).population.agg('sum').reset_index()
-    Geo = pd.merge(Geo, Pop, on=['county_fips', 'county_name', 'state_name'], how='inner')
-    Geo.columns = ['FIPS', 'County', 'State', 'lat', 'lng', 'population']
-
-    # extract US records and features
     data = data.loc[data.Country_Region == 'US', ['FIPS', 'Admin2', 'Province_State', 'Confirmed', 'Deaths']]
 
-    # rename features
+    zipinfo = pd.read_csv('uszips.csv')[['county_fips', 'county_name', 'state_name', 'population']].groupby(['county_fips', 'county_name', 'state_name']).population.agg('sum').reset_index()
+
+    # rename features and change format
     data.columns = ['FIPS', 'County', 'State', 'Confirmed', 'Deaths']
+    data.FIPS.fillna(0, inplace=True)
+    data.FIPS = data.FIPS.astype(int).astype(str).map(lambda x: '0'+x if len(x) == 4 and x != '0000' else x)    # remove decimal
+
+    zipinfo.columns = ['FIPS', 'County', 'State', 'population']
+    zipinfo.FIPS = zipinfo.FIPS.astype(str).map(lambda x: '0'+x if len(x) == 4 and x != '0000' else x)
 
     # remove 'City' in the County columns to match zipcode table
     data.County = data.County.str.replace(' City', '', regex=False)
 
     # final output features
-    feature = ['FIPS', 'County', 'State', 'Confirmed', 'Deaths']
+    feature = ['FIPS', 'County', 'State', 'population', 'Confirmed', 'Deaths']
 
     # extract non-missing records
-    x = pd.merge(data, Geo, on=['FIPS', 'County', 'State'], how='inner')[feature]
+    x = pd.merge(data, zipinfo, on=['FIPS', 'County', 'State'], how='inner')[feature]
 
     # correct wrong FIPS and fill in missing
     y = data[~data.FIPS.isin(x.FIPS)]
-    z = pd.merge(y, Geo, on=['County', 'State'], how='inner')
-    z['FIPS'] = z.FIPS_y
-    z = z[feature]
+    z = pd.merge(y[['County', 'State', 'Confirmed', 'Deaths']], zipinfo, on=['County', 'State'], how='inner')[feature]
 
     # merge two tables and sum same county records
-    data = pd.concat([x, z], ignore_index=True).groupby(['FIPS', 'County', 'State']).agg('sum').reset_index()
-
-    # merge Geo info
-    data = pd.merge(data, Geo, on=['FIPS', 'County', 'State'], how='inner')
+    data = pd.concat([x, z], ignore_index=True).groupby(['FIPS', 'County', 'State', 'population']).agg('sum').reset_index()
 
     return data
 
 def increment(x, y):
 
     # merge two current and previous dataset
-    temp = pd.merge(x, y[['FIPS', 'County', 'State', 'Confirmed', 'Deaths']], on=['FIPS', 'County', 'State'], how='left')
+    temp = pd.merge(x, y, on=['FIPS', 'County', 'State', 'population'], how='left')
 
     # fill NA for new county records
     temp.Confirmed_y.fillna(0, inplace=True)
@@ -57,16 +52,15 @@ def increment(x, y):
     temp['Deaths_new'] = temp.Deaths_x - temp.Deaths_y
     temp['Confirmed'] = temp.Confirmed_x
     temp['Deaths'] = temp.Deaths_x
+    temp['Confirmed_rate'] = temp.Confirmed / temp.population
+    temp['Deaths_rate'] = temp.Deaths / temp.Confirmed
 
-    # calucate confirmed and Death rate
-    temp.eval("Confirmed_rate = Confirmed / population", inplace=True)
-    temp.eval("Deaths_rate = Deaths / Confirmed", inplace=True)
+    # final output
+    feature = ['FIPS', 'County', 'State', 'Confirmed', 'Confirmed_new', 'Confirmed_rate', 'Deaths', 'Deaths_new', 'Deaths_rate']
 
     # remove useless features and save CSV
-    temp = temp[['FIPS', 'County', 'State', 'lat', 'lng',
-          'Confirmed', 'Confirmed_new', 'Deaths', 'Deaths_new', 'Confirmed_rate', 'Deaths_rate']]
-    temp.to_csv('covid_19_US.csv', index=False)
-    print(temp.shape)
+    temp[feature].to_excel('covid_19_US.xlsx', index=False)
+    print(temp[feature].shape)
 
 if __name__ == '__main__':
     path = '../../../COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/'
