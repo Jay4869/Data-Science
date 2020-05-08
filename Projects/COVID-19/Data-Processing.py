6a569589dc18db2a2a14ea53a6a7f9de42dab105
Jay4869 <jay4869@gmail.com>
2020-05-07 23:16:56 -0700

    add recovered metric

--- Projects/COVID-19/Data-Processing.py
+++ Projects/COVID-19/Data-Processing.py
@@ -8,13 +8,13 @@ def ETL(file):
     # loading COVID-19 data and zipcode
     print('Processing {}'.format(file[-14:]))
     data = pd.read_csv(file)
-    data = data.loc[data.Country_Region == 'US', ['FIPS', 'Admin2', 'Province_State', 'Confirmed', 'Deaths']]
+    data = data.query('Country_Region == "US"')[['FIPS', 'Admin2', 'Province_State', 'Confirmed', 'Deaths', 'Recovered']]
 
     zipinfo = pd.read_csv('uszips.csv')[['county_fips', 'county_name', 'state_name', 'population']].groupby(['county_fips', 'county_name', 'state_name']).population.agg('sum').reset_index()
 
     # rename features and change format
-    data.columns = ['FIPS', 'County', 'State', 'Confirmed', 'Deaths']
-    data.FIPS.fillna(0, inplace=True)
+    data.columns = ['FIPS', 'County', 'State', 'Confirmed', 'Deaths', 'Recovered']
+    data.fillna(0, inplace=True)
     data.FIPS = data.FIPS.astype(int).astype(str).map(lambda x: '0'+x if len(x) == 4 and x != '0000' else x)    # remove decimal
 
     zipinfo.columns = ['FIPS', 'County', 'State', 'population']
@@ -24,14 +24,14 @@ def ETL(file):
     data.County = data.County.str.replace(' City', '', regex=False)
 
     # final output features
-    feature = ['FIPS', 'County', 'State', 'population', 'Confirmed', 'Deaths']
+    feature = ['FIPS', 'County', 'State', 'population', 'Confirmed', 'Deaths', 'Recovered']
 
     # extract non-missing records
     x = pd.merge(data, zipinfo, on=['FIPS', 'County', 'State'], how='inner')[feature]
 
     # correct wrong FIPS and fill in missing
     y = data[~data.FIPS.isin(x.FIPS)]
-    z = pd.merge(y[['County', 'State', 'Confirmed', 'Deaths']], zipinfo, on=['County', 'State'], how='inner')[feature]
+    z = pd.merge(y[['County', 'State', 'Confirmed', 'Deaths', 'Recovered']], zipinfo, on=['County', 'State'], how='inner')[feature]
 
     # merge two tables and sum same county records
     data = pd.concat([x, z], ignore_index=True).groupby(['FIPS', 'County', 'State', 'population']).agg('sum').reset_index()
@@ -48,19 +48,23 @@ def increment(x, y):
     temp.Deaths_y.fillna(0, inplace=True)
 
     # calculate incremental increase
-    temp['Confirmed_new'] = temp.Confirmed_x - temp.Confirmed_y
-    temp['Deaths_new'] = temp.Deaths_x - temp.Deaths_y
-    temp['Confirmed'] = temp.Confirmed_x
-    temp['Deaths'] = temp.Deaths_x
-    temp['Confirmed_rate'] = temp.Confirmed / temp.population
-    temp['Deaths_rate'] = temp.Deaths / temp.Confirmed
+    temp.eval('Confirmed_new = Confirmed_x - Confirmed_y', inplace=True)
+    temp.eval('Deaths_new = Deaths_x - Deaths_y', inplace=True)
+    temp.eval('Recovered_new = Recovered_x - Recovered_y', inplace=True)
+    temp.eval('Confirmed = Confirmed_x', inplace=True)
+    temp.eval('Deaths = Deaths_x', inplace=True)
+    temp.eval('Recovered = Recovered_x', inplace=True)
+    temp.eval('Confirmed_rate = Confirmed_x / population', inplace=True)
+    temp.eval('Deaths_rate = Deaths_x / Confirmed_x', inplace=True)
+    temp.eval('Recovered_rate = Recovered_x / Confirmed_x', inplace=True)
 
     # final output
-    feature = ['FIPS', 'County', 'State', 'Confirmed', 'Confirmed_new', 'Confirmed_rate', 'Deaths', 'Deaths_new', 'Deaths_rate']
+    feature = ['FIPS', 'County', 'State', 'Confirmed', 'Deaths', 'Recovered', 'Confirmed_new', 'Deaths_new', 'Recovered_new', 'Confirmed_rate', 'Deaths_rate', 'Recovered_rate']
 
     # remove useless features and save CSV
-    temp[feature].to_excel('covid_19_US.xlsx', index=False)
-    print(temp[feature].shape)
+    temp = temp[feature]
+    temp.to_excel('covid_19_US.xlsx', index=False)
+    print(temp.shape)
 
 if __name__ == '__main__':
     path = '../../../COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/'
