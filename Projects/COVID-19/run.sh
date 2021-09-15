a433ae55d79a5d0c396ac1b9af3ed8729072233e
Jay4869 <jay4869@gmail.com>
2021-09-14 22:50:07 -0700

    update covid

--- Projects/COVID-19/run.sh
+++ Projects/COVID-19/run.sh
@@ -3,3 +3,9 @@ cd /d/Projects/COVID-19
 git pull
 cd /d/Projects/Data-Science/Projects/COVID-19/
 python Data-Processing.py
+git add covid_19_US.csv
+git add covid_states.csv
+git add Time_series_confirmed_cases.csv
+git add Time_series_confirmed_cases_states.csv
+git commit -m 'JHU daily refresh'
+git push
